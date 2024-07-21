518a93672b9c45d13bb0de088d3cfa76576e797e
schnee98 <jooeun06161@gmail.com>
2024-07-21 17:01:16 +0900

    refactor: 글로벌 스타일 적용 방식 변경

--- src/widgets/text/headers.tsx
+++ src/widgets/text/headers.tsx
@@ -1,11 +1,22 @@
 import { headerAnimation } from "shared/ui/animations";
 import styled from "styled-components";
 
+const HeaderItem = styled.h1`
+  font-family: "Cafe24Moyamoya-Regular-v1.0", cursive;
+  font-size: 4rem;
+  margin: 0;
+
+  @media (max-width: 650px) {
+    font-size: 2rem;
+  }
+`;
+
 export const Logo = styled.h1`
   animation: ${headerAnimation} 2s infinite linear;
   font-family: "Bungee Shade", cursive;
   text-align: center;
   font-size: 4rem;
+  margin: 0;
 
   @media (max-width: 650px) {
     font-size: 2rem;
@@ -22,3 +33,5 @@ export const HeaderText = styled.h1`
     font-size: 2rem;
   }
 `;
+
+export const Subtitle = styled.div``;
