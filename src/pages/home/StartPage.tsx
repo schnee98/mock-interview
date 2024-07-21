ae011d546aa641c1226fe806a811136e2a2b98ca
schnee98 <jooeun06161@gmail.com>
2024-07-22 00:57:01 +0900

    feat: fade-out 애니메이션 기능 추가

--- src/pages/home/StartPage.tsx
+++ src/pages/home/StartPage.tsx
@@ -1,48 +1,31 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styled, { keyframes } from "styled-components";
 import { StartButton } from "widgets/submit/buttons";
 import { Logo, Subtitle } from "widgets/text/headers";
 import BackgroundAnimation from "./BackgroundAnimation";
+import { FadeContent } from "shared/ui/animations";
 
 const StartPage: FC = () => {
+  const [isFadingOut, setIsFadingOut] = useState(false);
   const navigate = useNavigate();
 
-  const handleClick = () => navigate("/selection/developer");
+  const handleClick = () => {
+    setIsFadingOut(true);
+    setTimeout(() => navigate("/selection/developer"), 1500);
+  };
 
   return (
     <>
       <BackgroundAnimation />
-      <FadedConent order={0}>
+      <FadeContent order={0} isFadingOut={isFadingOut} delay={3}>
         <Logo>schuterview</Logo>
         <Subtitle>모 의 개 발 자 인 터 뷰</Subtitle>
-      </FadedConent>
-      <FadedConent order={1}>
+      </FadeContent>
+      <FadeContent order={1} isFadingOut={isFadingOut} delay={3}>
         <StartButton onClick={handleClick}>시작하기</StartButton>
-      </FadedConent>
+      </FadeContent>
     </>
   );
 };
-
-const fadeAnimation = keyframes`
-  0% {
-    opacity: 0;
-    transform: translateY(-30px);
-  }
-  100% {
-    opacity: 1;
-    transform: translateY(0);
-  }
-`;
-
-const FadedConent = styled.div<{ order: number }>`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  align-items: center;
-  opacity: 0;
-  transform: translateY(-30px);
-  animation: ${fadeAnimation} 1s calc(${({ order }) => order * 0.5}s + 3s) forwards;
-`;
 
 export default StartPage;
--- src/pages/selection/DeveloperSelection.tsx
+++ src/pages/selection/DeveloperSelection.tsx
@@ -1,28 +1,35 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Developer } from "shared/constants";
 import useSelectionStore from "shared/store/useSelectionStore";
+import { FadeContent } from "shared/ui/animations";
 import styled from "styled-components";
 import Checkbox from "widgets/checkbox/Checkbox";
 import { HeaderText } from "widgets/text/headers";
 
 const DeveloperSelection: FC = () => {
   const { setDeveloper } = useSelectionStore();
+  const [isFadingOut, setIsFadingOut] = useState(false);
   const navigate = useNavigate();
 
   const handleCheckboxChange = (value: Developer) => {
     setDeveloper(value);
-    navigate("/selection/theme");
+    setIsFadingOut(true);
+    setTimeout(() => navigate("/selection/theme"), 1500);
   };
 
   return (
     <>
-      <HeaderText>당신은 어떤 개발자인가요?</HeaderText>
-      <Selection>
-        <Checkbox label="백엔드" img="/img/java.svg" onClick={() => handleCheckboxChange("backend")} />
-        <Checkbox label="프론트엔드" img="/img/javascript.svg" onClick={() => handleCheckboxChange("frontend")} />
-        <Checkbox label="IOS" img="/img/swift.svg" onClick={() => handleCheckboxChange("ios")} />
-      </Selection>
+      <FadeContent order={0} isFadingOut={isFadingOut}>
+        <HeaderText>당신은 어떤 개발자인가요?</HeaderText>
+      </FadeContent>
+      <FadeContent order={1} isFadingOut={isFadingOut}>
+        <Selection>
+          <Checkbox label="백엔드" img="/img/java.svg" onClick={() => handleCheckboxChange("backend")} />
+          <Checkbox label="프론트엔드" img="/img/javascript.svg" onClick={() => handleCheckboxChange("frontend")} />
+          <Checkbox label="IOS" img="/img/swift.svg" onClick={() => handleCheckboxChange("ios")} />
+        </Selection>
+      </FadeContent>
     </>
   );
 };
